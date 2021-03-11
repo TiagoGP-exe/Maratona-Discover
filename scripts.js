5bca7e403c3272ffc6e5b541c33bd8f960406ce9
Tiago Guimarães Pinto <45266215+TiagoGP-exe@users.noreply.github.com>
2021-03-11 18:05:38 -0400

    bug
    Quando era adicionado "-" o valor repetia o sinal

--- scripts.js
+++ scripts.js
@@ -1,42 +1,43 @@
 const Modal = {
-    open (){
-        //abrir modal
-        //adicionar o class active ao modal
-        document.querySelector('.modal-overlay')
-        .classList.add('active')
+    open(){
+        // Abrir modal
+        // Adicionar a class active ao modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .add('active')
+
     },
     close(){
-        //fechar modal
-        //remover a class active do modal
-        document.querySelector('.modal-overlay')
-        .classList.remove('active')
+        // fechar o modal
+        // remover a class active do modal
+        document
+            .querySelector('.modal-overlay')
+            .classList
+            .remove('active')
     }
 }
 
 const Storage = {
-    get(){
-        return JSON.parse(localStorage.getItem('dev.finances:transactions')) || []
+    get() {
+        return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
     },
 
-    set(transactions){
+    set(transactions) {
         localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
     }
 }
 
-//Eu preciso somar as entradas
-//depois eu preciso somar as saídas e remover 
-//das entradas o valor da saídas
-//assim, eu terei o total
-
 const Transaction = {
-    all: Storage.get(),  
+    all: Storage.get(),
+
     add(transaction){
         Transaction.all.push(transaction)
-        console.log(Transaction.all)
+
         App.reload()
     },
 
-    remove(index){
+    remove(index) {
         Transaction.all.splice(index, 1)
 
         App.reload()
@@ -44,53 +45,90 @@ const Transaction = {
 
     incomes() {
         let income = 0;
-        //pegar todas as tansações 
-        //para cada transação 
-        Transaction.all.forEach(Transaction => {
-            //se for mairo que zero
-            if(Transaction.amount > 0){
-                income += Transaction.amount;
+        Transaction.all.forEach(transaction => {
+            if( transaction.amount > 0 ) {
+                income += transaction.amount;
             }
-            //somar a uma variavel e retornar a variavel
         })
         return income;
     },
+
     expenses() {
         let expense = 0;
-        //pegar todas as tansações 
-        //para cada transação 
-        Transaction.all.forEach(Transaction => {
-            //se for mairo que zero
-            if(Transaction.amount < 0){
-                expense += Transaction.amount;
+        Transaction.all.forEach(transaction => {
+            if( transaction.amount < 0 ) {
+                expense += transaction.amount;
             }
-            //somar a uma variavel e retornar a variavel
         })
         return expense;
     },
+
     total() {
         return Transaction.incomes() + Transaction.expenses();
     }
 }
 
-//Sbustituir os dados do HTML com os dados do JS
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
 
-const Utils ={
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+
+    innerHTMLTransaction(transaction, index) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount)
+
+        const html = `
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSclass}">${amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td>
+            <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
+        </td>
+        `
+
+        return html
+    },
+
+    updateBalance() {
+        document
+            .getElementById('incomeDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.incomes())
+        document
+            .getElementById('expenseDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.expenses())
+        document
+            .getElementById('totalDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.total())
+    },
+
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = ""
+    }
+}
+
+const Utils = {
     formatAmount(value){
-        value = value * 100
+        value = Number(value.replace(/\,\./g, "")) * 100
         
-        return Math.round(value)
+        return value
     },
 
-    formatDate(date){
-        const splitteDate = date.split("-")
-        return `${splitteDate[2]}/${splitteDate[1]}/${splitteDate[0]}`
+    formatDate(date) {
+        const splittedDate = date.split("-")
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
     },
 
-    formatCurrency(value){
+    formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : ""
 
-        value =  String(value).replace(/\,?\.?/g,"")
+        value = String(value).replace(/\D/g, "")
 
         value = Number(value) / 100
 
@@ -99,7 +137,7 @@ const Utils ={
             currency: "BRL"
         })
 
-        return (signal + value)
+       return signal + value
     }
 }
 
@@ -109,9 +147,9 @@ const Form = {
     date: document.querySelector('input#date'),
 
     getValues() {
-        return{
+        return {
             description: Form.description.value,
-            amount:Form.amount.value,
+            amount: Form.amount.value,
             date: Form.date.value
         }
     },
@@ -126,13 +164,12 @@ const Form = {
         }
     },
 
-    formatValues(){
-        let {description, amount, date}  = Form.getValues()
-
+    formatValues() {
+        let { description, amount, date } = Form.getValues()
+        
         amount = Utils.formatAmount(amount)
 
         date = Utils.formatDate(date)
-        console.log(`description: ${description}, date: ${date}, amount: ${amount}`)
 
         return {
             description,
@@ -140,8 +177,8 @@ const Form = {
             date
         }
     },
-    
-    clearFileds(){
+
+    clearFields() {
         Form.description.value = ""
         Form.amount.value = ""
         Form.date.value = ""
@@ -167,54 +204,16 @@ const Form = {
     }
 }
 
-const DOM = {
-    TransactionsContainer: document.querySelector('#data-table tbody'),
-
-    addTransaction(Transaction, index){
-        const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTrasnsaction(Transaction, index)
-        tr.dataset.index = index
-        DOM.TransactionsContainer.appendChild(tr)
-    },
-    innerHTMLTrasnsaction(Transaction, index){
-        const CSSclass = Transaction.amount > 0 ? "income" : "expense"
-
-        const amount = Utils.formatCurrency(Transaction.amount)
-
-        const html = 
-       `<td class="descriction">${Transaction.description}</td>
-        <td class="${CSSclass}">${amount}</td>
-        <td class="date">${Transaction.date}</td>
-        <td>
-            <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
-        </td>`
-
-        return html
-    },
-    upadateBalance(){
-        document.getElementById('incomeDisplay')
-        .innerHTML = Utils.formatCurrency(Transaction.incomes())
-        document.getElementById('expenseDisplay')
-        .innerHTML = Utils.formatCurrency(Transaction.expenses())
-        document.getElementById('totalDisplay')
-        .innerHTML = Utils.formatCurrency(Transaction.total())
-    },
-    clearTransaction(){
-        DOM.TransactionsContainer.innerHTML = ""
-    }
-}
-
 const App = {
-    init(){
-
+    init() {
         Transaction.all.forEach(DOM.addTransaction)
         
-        DOM.upadateBalance()
-        
+        DOM.updateBalance()
+
         Storage.set(Transaction.all)
     },
-    reload(){
-        DOM.clearTransaction()
+    reload() {
+        DOM.clearTransactions()
         App.init()
     },
 }
